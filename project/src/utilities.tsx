64e9aed275fb0c70a9954dbec0c442c4a349f118
Kevin Connor <kevinpaulconnor@gmail.com>
2022-01-19 20:59:09 -0600

    playerrow sort order by last name
    resolves #3

--- project/src/utilities.tsx
+++ project/src/utilities.tsx
@@ -15,7 +15,16 @@ export const generatePlayerRows = (appState: AppState, rosterStatus: RosterStatu
     let ret = <Fragment/>;
     if (roster && roster.players.length > 0) {
         let playerRows :JSX.Element[] = [];
-        const filteredPlayers = roster.players.filter(player => player.status === rosterStatus);
+        const filteredPlayers = roster.players.filter(player => player.status === rosterStatus)
+            .sort((a, b) => {
+                if (a.last_name > b.last_name) {
+                    return 1
+                }
+                if (b.last_name > a.last_name) {
+                    return -1;
+                }
+                return 0;
+            });
         filteredPlayers.forEach(player => {
             playerRows.push(<PlayerRow
                 key={player.id}
